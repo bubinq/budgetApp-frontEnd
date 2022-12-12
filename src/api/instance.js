9b4689a517dc129340f8377c58e810d70cd598dc
bubinq <emilbumov@gmail.com>
2022-12-12 17:55:47 +0200

    Added API ref

--- src/api/instance.js
+++ src/api/instance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const instance = axios.create({
-  baseURL: "http://localhost:8000/",
+  baseURL: "https://budgetapp-react-backend.onrender.com/",
 });
 
 export const getLastThreeMonthsAmounts = async (year, month) => {
